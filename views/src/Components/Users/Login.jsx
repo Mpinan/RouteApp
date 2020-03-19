b9e961c6747d918bbbc13abaa215e56588efa439
LeoRoma <xiajtn@gmail.com>
2020-03-19 17:48:48 +0000

    login can redirect with home
    Co-authored-by: jaitone <jaitoone@gmail.com>

--- views/src/Components/Users/Login.jsx
+++ views/src/Components/Users/Login.jsx
@@ -1,41 +1,83 @@
 import React, { Component } from "react";
 import { Button, Col, Form, FormGroup, Label, Input } from "reactstrap";
+import { Redirect } from 'react-router-dom';
 
-const Login = () => {
-  return (
-    <Form style={{ margin: "50px 0" }}>
-      <FormGroup row>
-        <Label for="exampleEmail" sm={2} size="lg">
-          Email
-        </Label>
-        <Col sm={10}>
-          <Input
-            type="email"
-            name="email"
-            id="exampleEmail"
-            placeholder="email"
-            bsSize="lg"
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="exampleEmail2" sm={2}>
-          Password
-        </Label>
-        <Col sm={10}>
-          <Input
-            type="password"
-            name="password"
-            id="exampleEmail2"
-            placeholder="password"
-          />
-        </Col>
-      </FormGroup>
-      <Button color="secondary" size="lg" block>
-        Log in
-      </Button>
-    </Form>
-  );
+class Login extends Component {
+
+  state = {
+    redirect: false,
+    email: '',
+    password: ''
+  }
+
+
+  handlePassword = event => {
+    this.setState({
+      password: event.target.value
+    })
+  };
+
+  handleEmail = event => {
+    this.setState({
+      email: event.target.value
+    })
+  };
+
+  handleLogin = event => {
+    this.setRedirect();
+  };
+
+  setRedirect() {
+    this.setState({
+      redirect: true
+    })
+  };
+
+  renderRedirect() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />
+    }
+  };
+
+  render() {
+    return (
+      <Form style={{ margin: "50px 0" }}>
+        {this.renderRedirect()}
+        <FormGroup row>
+          <Label for="exampleEmail" sm={2} size="lg">
+            Email
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="email"
+              bsSize="lg"
+              onChange={this.handleEmail.bind(this)}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleEmail2" sm={2}>
+            Password
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="password"
+              name="password"
+              id="exampleEmail2"
+              placeholder="password"
+              onChange={this.handlePassword.bind(this)}
+            />
+          </Col>
+        </FormGroup>
+        <Button color="secondary" size="lg" block onClick={this.handleLogin.bind(this)}>
+          Log in
+        </Button>
+      </Form>
+    );
+  }
 };
 
 export default Login;
