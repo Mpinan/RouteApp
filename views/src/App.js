8725dfa0c4b2156ff141596316f38184fbdd63a4
Mario90 <mgpinan1990@gmail.com>
2020-03-25 17:09:18 +0000

    Added google functionality

--- views/src/App.js
+++ views/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Container } from "reactstrap";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import SignUp from "./Components/Users/Signup";
 import Login from "./Components/Users/Login";
 import MainPage from "./Components/mainPage";
 import UserPage from "./Components/Users/UserPage";
+import NotFound from "./Components/notFound";
 
 class App extends Component {
   state = {};
@@ -17,7 +18,9 @@ class App extends Component {
           <Route path="/signup" component={SignUp} />
           <Route path="/login" component={Login} />
           <Route path="/userPage" component={UserPage} />
+          <Route path="/not-found" component={NotFound}></Route>
         </BrowserRouter>
+        {/* <Redirect to="/not-found" /> */}
       </Container>
     );
   }
--- views/src/Components/Users/UserPage.jsx
+++ views/src/Components/Users/UserPage.jsx
@@ -1,8 +1,60 @@
-import React, { Component } from "react";
+import React from "react";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
-class UserPage extends Component {
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.isMarkerShown && (
+      <Marker
+        position={{ lat: -34.397, lng: 150.644 }}
+        onClick={props.onMarkerClick}
+      />
+    )}
+  </GoogleMap>
+));
+
+class UserPage extends React.PureComponent {
+  state = {
+    isMarkerShown: false
+  };
+
+  componentDidMount() {
+    this.delayedShowMarker();
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true });
+    }, 3000);
+  };
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false });
+    this.delayedShowMarker();
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    );
   }
 }
 
