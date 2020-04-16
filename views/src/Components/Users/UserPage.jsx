b2075218ffbc46da0cdd2470ac4be2cbb617d7ba
Mario90 <mgpinan1990@gmail.com>
2020-04-16 18:49:14 +0100

    Added function to calculate distance

--- views/src/Components/Users/UserPage.jsx
+++ views/src/Components/Users/UserPage.jsx
@@ -13,68 +13,105 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 const apiKey = "AIzaSyBIGLbrD_tHjQZFi1GQ61wRi_ltzkJ8w3A";
 
 export class MapContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    const { lat, lng } = this.props.initialCenter;
-    this.state = {
-      darkMode: false
-    };
+  onMarkerClick() {
+    console.log("hello");
+  }
+
+  calculateDistance() {
+    const { google } = this.props;
+    const service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+      {
+        origins: [{ lat: 55.93, lng: -3.118 }],
+        destinations: [{ lat: 50.087, lng: 14.421 }],
+        travelMode: "DRIVING"
+      },
+      (response, status) => {
+        console.log("response", response);
+        console.log("status", status);
+      }
+    );
   }
 
   render() {
-    console.log(this.props);
-    // const style = {
-    //   width: "100%",
-    //   height: "100%",
-    //   position: "relative"
-    // };
+    const { posts, latitude, longitude, google } = this.props;
+
     return (
       <Container>
         <div>
           <div className="container-fluid border-bottom" id="map">
             <div id="map">
-              <Map google={this.props.google} zoom={14}>
-                <Marker
-                  onClick={this.onMarkerClick}
-                  name={"Current location"}
-                />
-
-                <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
+              <Map
+                google={google}
+                // initialCenter={{
+                //   lat: Number(latitude),
+                //   lng: Number(longitude)
+                // }}
+                onClick={this.mapClicked}
+                zoom={14}
+              >
+                {/* {posts.lenght
+                  ? posts.map(post => {
+                      return (
+                        <Marker
+                          key={post.id}
+                          title={post.location.name}
+                          name={post.location.name}
+                          onClick={this.onMarkerClick}
+                          position={{
+                            lat: post.location.latitude,
+                            lng: post.location.longitude
+                          }}
+                        />
+                      );
+                    })
+                  : null} */}
+                <InfoWindow
+                // marker={this.state.activeMarker}
+                // visible={this.state.showingInfoWindow}
+                >
+                  <div className="marker">
+                    <a href="">
+                      {/* <h2>{this.state.selectedPlace.name}</h2> */}
+                    </a>
+                    {/* <img src={`${this.findUrl()}`} className="infoWindow-pic" /> */}
+                  </div>
+                  {this.calculateDistance()}
+                </InfoWindow>
               </Map>
             </div>
           </div>
         </div>
-        <div>
-          <Container>
-            <div>
-              <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                  <h3>
-                    <Badge>From</Badge>
-                  </h3>
-                </InputGroupAddon>
-                <Input />
-              </InputGroup>
-              <InputGroup>
-                <Input />
-                <InputGroupAddon>
-                  <h3>
-                    <Badge>To</Badge>
-                  </h3>
-                </InputGroupAddon>
-              </InputGroup>
 
-              <CustomInput
-                type="radio"
-                id="exampleCustomRadio"
-                name="customRadio"
-                label="Click to save this route"
-              >
-                <Button style={{ marginLeft: "5%" }}>Route it!</Button>
-              </CustomInput>
-            </div>
-          </Container>
-        </div>
+        <Container>
+          {/* <div style={{ marginBottom: "40px" }}>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">
+                <h3>
+                  <Badge>From</Badge>
+                </h3>
+              </InputGroupAddon>
+              <Input />
+            </InputGroup>
+            <InputGroup>
+              <Input />
+              <InputGroupAddon>
+                <h3>
+                  <Badge>To</Badge>
+                </h3>
+              </InputGroupAddon>
+            </InputGroup>
+
+            <CustomInput
+              type="radio"
+              id="exampleCustomRadio"
+              name="customRadio"
+              label="Click to save this route"
+            >
+              <Button style={{ marginLeft: "5%" }}>Route it!</Button>
+            </CustomInput>
+          </div> */}
+        </Container>
       </Container>
     );
   }
