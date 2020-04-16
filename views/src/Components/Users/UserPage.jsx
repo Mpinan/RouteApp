92212ebe79c0529157a7ac6b6496394d9b3b5df3
Mario90 <mgpinan1990@gmail.com>
2020-04-16 16:29:42 +0100

    Adjusted google map to the screen

--- views/src/Components/Users/UserPage.jsx
+++ views/src/Components/Users/UserPage.jsx
@@ -13,50 +13,69 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 const apiKey = "AIzaSyBIGLbrD_tHjQZFi1GQ61wRi_ltzkJ8w3A";
 
 export class MapContainer extends React.Component {
-  render() {
-    const style = {
-      width: "62.40%",
-      height: "62.5%"
+  constructor(props) {
+    super(props);
+    const { lat, lng } = this.props.initialCenter;
+    this.state = {
+      darkMode: false
     };
+  }
+
+  render() {
+    console.log(this.props);
+    // const style = {
+    //   width: "100%",
+    //   height: "100%",
+    //   position: "relative"
+    // };
     return (
-      <div className="container-fluid border-bottom">
-        <div className="map">
-          <Map google={this.props.google} zoom={14} style={style}>
-            <Marker onClick={this.onMarkerClick} name={"Current location"} />
+      <Container>
+        <div>
+          <div className="container-fluid border-bottom" id="map">
+            <div id="map">
+              <Map google={this.props.google} zoom={14}>
+                <Marker
+                  onClick={this.onMarkerClick}
+                  name={"Current location"}
+                />
 
-            <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>{/* <h1>{this.state.selectedPlace.name}</h1> */}</div>
-            </InfoWindow>
-          </Map>
+                <InfoWindow onClose={this.onInfoWindowClose}></InfoWindow>
+              </Map>
+            </div>
+          </div>
         </div>
-        <div style={{ marginTop: "5%", marginBottom: "2%" }}>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend">
-              <h3>
-                <Badge>From</Badge>
-              </h3>
-            </InputGroupAddon>
-            <Input />
-          </InputGroup>
-          <InputGroup>
-            <Input />
-            <InputGroupAddon>
-              <h3>
-                <Badge>To</Badge>
-              </h3>
-            </InputGroupAddon>
-          </InputGroup>
+        <div>
+          <Container>
+            <div>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <h3>
+                    <Badge>From</Badge>
+                  </h3>
+                </InputGroupAddon>
+                <Input />
+              </InputGroup>
+              <InputGroup>
+                <Input />
+                <InputGroupAddon>
+                  <h3>
+                    <Badge>To</Badge>
+                  </h3>
+                </InputGroupAddon>
+              </InputGroup>
 
-          <CustomInput
-            type="radio"
-            id="exampleCustomRadio"
-            name="customRadio"
-            label="Click to save this route"
-          >
-            <Button style={{ marginLeft: "5%" }}>Route it!</Button>
-          </CustomInput>
+              <CustomInput
+                type="radio"
+                id="exampleCustomRadio"
+                name="customRadio"
+                label="Click to save this route"
+              >
+                <Button style={{ marginLeft: "5%" }}>Route it!</Button>
+              </CustomInput>
+            </div>
+          </Container>
         </div>
-      </div>
+      </Container>
     );
   }
 }
