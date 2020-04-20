61160ce5756d4211260005f19cf58a03cbb9b008
Mario90 <mgpinan1990@gmail.com>
2020-04-20 09:25:52 +0100

    Added not found route

--- views/src/App.js
+++ views/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             <Route exact path="/home" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/userpage" component={UserPage} />
-            <Route exact path="/not-found" component={NotFound} />
+            <Route path="/*" component={NotFound} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/about" component={About} />
