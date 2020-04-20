30eb9f1980dc156de4616fd3bdb65ff57b752844
Mario90 <mgpinan1990@gmail.com>
2020-04-20 09:27:50 +0100

    Added route to the last of the routes to avoid routing problems

--- views/src/App.js
+++ views/src/App.js
@@ -26,11 +26,11 @@ class App extends Component {
             <Route exact path="/home" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/userpage" component={UserPage} />
-            <Route path="/*" component={NotFound} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/about" component={About} />
             <Redirect exact path="/" exact to="/home" />
+            <Route path="/*" component={NotFound} />
           </Switch>
         </BrowserRouter>
       </div>
